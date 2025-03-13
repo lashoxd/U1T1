cf4ce92df619fe1215bf6d977ab032a38c60f22f
Yun <huangyun.ai@gmail.com>
2025-03-13 12:04:34 -0300

    versión 1

---
+++ main.py
@@ -0,0 +1,33 @@
+'''
+No cambies las definiciones (nombres y argumentos) de las funciones dadas aquí.
+Por ahora, abordar las excepciones (p.ej., comprobar si el argumento es un número, 
+    no una cadena cuando la función es para sumar los argumentos) es opcional.
+'''
+
+# Ejercicio 1
+def calcular_puntaje(N, C):
+    ...
+
+# Ejercicio 2
+# Asumimos que x dado es un número positivo.
+def es_primo(x):
+    ...
+
+# Ejercicio 3
+def imprimir_parejas(nombres):
+    ...
+
+# Ejercicio 4
+def obtener_asiento(nombre):
+    # Parte 1
+    ...
+    # Parte 2
+    ...
+
+# Para llamar y comprobar tus funciones
+def main():
+    ...
+
+# No elimines esta parte que es para evitar que se ejecute el código cuando sea importado como un módulo.
+if __name__ == "__main__":
+    main()
---
+++ opcional.py
@@ -0,0 +1,22 @@
+'''
+No cambies las definiciones (nombres y argumentos) de las funciones dadas aquí.
+Por ahora, abordar las excepciones (p.ej., comprobar si el argumento es un número, 
+    no una cadena cuando la función es para sumar los argumentos) es opcional.
+'''
+
+# Ejercicio 1
+def contar_vocales(palabra):
+    ...
+
+# Ejercicio 2
+def calcular_suma_digitos(numero):
+    ...
+
+
+# Para llamar y comprobar tus funciones
+def main():
+    ...
+
+# No elimines esta parte que es para evitar que se ejecute el código cuando sea importado como un módulo.
+if __name__ == "__main__":
+    main()
